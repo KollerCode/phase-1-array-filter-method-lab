45edbfa75ebd38a8598ed2efd334b4c8a0dae33b
Natasha Koller <kollernatasha@gmail.com>
2021-12-14 18:22:40 -0500

    commit tree 8bb6e3b6dfb6f75a8c7cb1cc99483e26fa2b3165

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solution here
+function findMatching(drivers, string){
+    let matchingDrivers = drivers.filter(function (driver) {
+        return driver.toLowerCase() === string.toLowerCase()
+    })
+    return matchingDrivers
+}
+
+function fuzzyMatch(drivers, string){
+    let result = drivers.filter(function (driver) {
+        return driver.startsWith(string)
+      });
+      
+    return result
+}
+
+function matchName(drivers,nameFound){
+    let driversNames = drivers.filter(function(object){
+        return object.name === nameFound
+    })
+    return driversNames
+}
